572406842f461298e88f8c5dba004917e5ed8cb8
pinhello <lee123lee123@163.com>
2023-03-19 04:39:15 +0800

    feat:  格式化

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,23 +1,22 @@
 // /api/xxx
-import type { NextRequest } from "next/server";
+import type {NextRequest} from "next/server";
 
 export default async function handler(req: NextRequest) {
-    const url = new URL(req.url)
-
-    const modUrl = `https://api.openai.com${url.pathname.replace("\/api", "")}`
-    console.log(modUrl)
-    const modReq = new Request(modUrl, {
-        method: req.method,
-        headers: req.headers,
-        body: req.body,
-        redirect: 'follow'
-    })
+    const url = new URL(req.url);
+    const modUrl = `https://api.openai.com${url.pathname.replace("\/api", "")}`;
     try {
-        const resp = await fetch(modReq);
+        const resp = await fetch(new Request(modUrl, {
+                    method: req.method,
+                    headers: req.headers,
+                    body: req.body,
+                    redirect: 'follow',
+                }
+            )
+        );
         const modResp = new Response(resp.body, resp);
         modResp.headers.set('Access-Control-Allow-Origin', '*');
         return modResp;
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
 }
--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,15 +1,14 @@
 // /api/chat
-import type { NextRequest } from "next/server";
+import type {NextRequest} from "next/server";
 
 export default async function handler(req: NextRequest) {
-    const modReq = new Request('https://api.openai.com/v1/chat/completions', {
-        method: req.method,
-        headers: req.headers,
-        body: req.body,
-        redirect: 'follow'
-    })
     try {
-        const resp = await fetch(modReq);
+        const resp = await fetch(new Request('https://api.openai.com/v1/chat/completions', {
+            method: req.method,
+            headers: req.headers,
+            body: req.body,
+            redirect: 'follow',
+        }));
         const modResp = new Response(resp.body, resp);
         modResp.headers.set('Access-Control-Allow-Origin', '*');
         return modResp;
