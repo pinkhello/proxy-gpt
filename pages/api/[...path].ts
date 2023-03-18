e0008cc252104f2ae1a6df1065dfbbb36d655df7
pinhello <lee123lee123@163.com>
2023-03-19 04:17:08 +0800

    feat: 注释掉错误的

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,29 +1,29 @@
-// /api/xxx
-
-import type { NextRequest } from "next/server";
-
-export const config = {
-    runtime: 'edge',
-}
-
-
-export default async function handler(req: NextRequest) {
-    const url = new URL(req.url)
-
-    const modUrl = `https://api.openai.com${url.pathname.replace("\/api", "")}`
-    console.log(modUrl)
-    const modReq = new Request(modUrl, {
-        method: req.method,
-        headers: req.headers,
-        body: req.body,
-        redirect: 'follow'
-    })
-    try {
-        const resp = await fetch(modReq);
-        const modResp = new Response(resp.body, resp);
-        modResp.headers.set('Access-Control-Allow-Origin', '*');
-        return modResp;
-    } catch (e) {
-        console.log(e)
-    }
-}
+// // /api/xxx
+//
+// import type { NextRequest } from "next/server";
+//
+// export const config = {
+//     runtime: 'edge',
+// }
+//
+//
+// export default async function handler(req: NextRequest) {
+//     const url = new URL(req.url)
+//
+//     const modUrl = `https://api.openai.com${url.pathname.replace("\/api", "")}`
+//     console.log(modUrl)
+//     const modReq = new Request(modUrl, {
+//         method: req.method,
+//         headers: req.headers,
+//         body: req.body,
+//         redirect: 'follow'
+//     })
+//     try {
+//         const resp = await fetch(modReq);
+//         const modResp = new Response(resp.body, resp);
+//         modResp.headers.set('Access-Control-Allow-Origin', '*');
+//         return modResp;
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // /api/hello
-import type { NextRequest} from "next/server";
+import type { NextRequest } from "next/server";
 
 export const config = {
     runtime: 'edge',
 }
-
 
 export default async function handler(req: NextRequest) {
     return new Response(
